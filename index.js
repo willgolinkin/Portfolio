24dcb166200ba8060cf25bdfa0d5fe1d02e038d9
Will Golinkin <will.golinkin@gmail.com>
2019-06-04 18:34:56 -0400

    moderately functional javacript

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let projectNumber = 0;
 
+//access and add project information from STORE
 function generateProject () {
     if (projectNumber < STORE.length) {
-        return `<section class="${projectNumber}"
+        return `<div class= ${STORE[projectNumber].project}>
         <div class="project-description">
-            <h1 class="js-title">${STORE[projectNumber].title}<!--USMC Quiz App--></h1>
+            <h1 class="js-title">${STORE[projectNumber].title}</h1>
             <p class="js-description">
-                ${STORE[projectNumber].description};
+                ${STORE[projectNumber].description}
             </p>
             <div class = "btn-group">
                 <button class="github">Github Repo</button>
@@ -14,38 +15,50 @@ function generateProject () {
             </div>
         </div>
         <div class="screenshot-container">
-            <img src= ${STORE[projectNumber].screenshot_one}>;
-            <img src= ${STORE[projectNumber].screenshot_two}>;
+            <img src= ${STORE[projectNumber].screenshot_one}>
+            <img src= ${STORE[projectNumber].screenshot_two}>
         </div>
-    </section>`
+    </div>`
     } else {
         returnToHomePage ();
     }
 }
 
+//once I have additional projects, increment in order to display next project
 function changeProjectNumber () {
     projectNumber ++;
 }
 
+//hide about section
+//unhide/show project div
 function viewProject () {
     $('.about').on('click', '.view_projects_button', function(event) {
         $('.about').remove();
+        //not sure if this adds value for this script
         $('.project').css('display', 'block');
-    })
+    });
 }
 
+//render first project to the DOM
 function renderProject () {
-    $('.project').html(generateProject());
+  $('.project').html(generateProject());
 }
 
 function renderNextProject () {
     console.log('going to next project');
+    //add event listener for click of next project and generate next project html
+}
+
+function returnToHomePage () {
+    console.log('returning to Home Page');
+    //add event listener for click of home page button and reload home page
 }
 
 function showProjects () {
-    viewProjct ();
+    viewProject ();
     renderProject ();
     renderNextProject ();
+    returnToHomePage ();
 }
 
 $(showProjects);
