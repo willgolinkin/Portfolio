fd8edf7c81d595a894fd7329aedaff98b781f620
Will Golinkin <will.golinkin@gmail.com>
2019-06-27 15:44:21 -0400

    added new project to STORE

--- datastore.js
+++ datastore.js
@@ -1,5 +1,18 @@
 const STORE = [
     {
+        project_div: "Project 2",
+        title: "EatKlean",
+        technologies_used: 
+        `<li><i class="fab fa-html5"></i></li>
+        <li><i class="fab fa-js"></i></li>
+        <li><i class="fab fa-css3-alt"></i></li>`,
+        description: "The EatKlean app allows you to track your food and exercise using the Nutritionix API",
+        github_repo: "https://github.com/willgolinkin/Nutritionix-API-Hack-Capstone",
+        web_page: "https://willgolinkin.github.io/Nutritionix-API-Hack-Capstone/",
+        screenshot_one: "Img Assets\\EatKlean Search Results Screenshot_compressed.png",
+        screenshot_two: "Img Assets\\EatKlean Home Page Screenshot_compressed.png"
+    },
+    {
         project_div: "Project 1",
         title: "USMC Quiz App",
         technologies_used: 
@@ -11,19 +24,5 @@ const STORE = [
         web_page: "https://willgolinkin.github.io/interactive_USMC_quiz_app/",
         screenshot_one: "Img Assets\\Marine Corps Quiz App Start Page Screenshot Compressed.png",
         screenshot_two: "Img Assets\\Marine Corps Quiz App Question Page Screenshot Compressed.png",
-    },
-    {
-        project_div: "Project 2",
-        title: "EatKlean",
-        technologies_used: 
-        `<li><i class="fab fa-html5"></i></li>
-        <li><i class="fab fa-js"></i></li>
-        <li><i class="fab fa-css3-alt"></i></li>`,
-        description: "The EatKlean app allows you to track your food and exercise using the Nutritionix API",
-        github_repo: "https://github.com/willgolinkin/Nutritionix-API-Hack-Capstone",
-        web_page: "https://willgolinkin.github.io/Nutritionix-API-Hack-Capstone/",
-        screenshot_one: "Img Assets\\Marine Corps Quiz App Start Page Screenshot Compressed.png",
-        screenshot_two: "Img Assets\\Marine Corps Quiz App Question Page Screenshot Compressed.png", 
     }
-
 ];
